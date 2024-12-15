462bca6b4dc7b4a8d1e825acb0b94c8a8b494851
Axel Lells Hjelmqvist <axelhjq@gmail.com>
2024-12-15 18:18:35 +0100

    Limit println

--- aoc/main/Day3.scala
+++ aoc/main/Day3.scala
@@ -21,23 +21,15 @@ object Day3:
         var on = true
         """(mul)\(([\d]{1,3}),([\d]{1,3})\)|(do\(\))|(don't\(\))""".r.findAllMatchIn(content)
             .map((m: Regex.Match) =>
-                // println(m.group(0))
-                // val c = m.group(1)
                 if on && m.group(1) == "mul" then
-                    println("ON")
                     m.group(2).toInt * m.group(3).toInt
                 else if m.group(0) == "do()" then
-                    println(m.group(0))
-                    println("Set on")
                     on = true
                     0
                 else if m.group(0) == "don't()" then
-                    println(m.group(0))
-                    println("Set off")
                     on = false
                     0
                 else
-                    println("OFF")
                     0
             )
             .sum
